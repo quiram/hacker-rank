a0bb54fcbd81d94304d7a824bbf82b639ae7f0fb
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-03-04 14:42:44 -0800

    Exercise Time Conversion

---
+++ src/main/java/com/github/quiram/hackerrank/timeconversion/Solution.java
@@ -0,0 +1,44 @@
+package com.github.quiram.hackerrank.timeconversion;
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+        int hour = Integer.parseInt(s.substring(0, 2)) % 12;
+        boolean pm = s.charAt(8) == 'P';
+        if (pm) {
+            hour = hour + 12;
+        }
+
+        return String.format("%02d%s", hour, s.substring(2, 8));
+    }
+
+}
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/one-week-preparation-kit-time-conversion/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one">Time Conversion</a>
+ */
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
