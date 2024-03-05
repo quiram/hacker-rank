d869843088aa46032e72f999af0e0b8cef11867c
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-03-05 10:24:31 -0800

    fix exercise Zig Zag Sequence

---
+++ src/test/java/com/github/quiram/hackerrank/basic/zigzagsequence/MainTest.java
@@ -0,0 +1,53 @@
+package com.github.quiram.hackerrank.basic.zigzagsequence;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @ParameterizedTest
+    @MethodSource("scenarios")
+    void findZigZagSequence(int[] a) {
+        Main.findZigZagSequence(a, a.length);
+        final List<Integer> output = Arrays.stream(outContent.toString().trim().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(toList());
+        IntStream.range(1, output.size() / 2)
+                .forEach(i -> assertTrue(() -> output.get(i) > output.get(i - 1), format("list: %s; index: %s", output, i)));
+        IntStream.range(output.size() / 2 + 1, output.size())
+                .forEach(i -> assertTrue(() -> output.get(i) < output.get(i - 1), format("list: %s; index: %s", output, i)));
+    }
+
+    public static Stream<int[]> scenarios() {
+        return Stream.of(
+                new int[]{2, 3, 5, 1, 4}
+        );
+    }
+}
