546543316541db9a900d9cd0220bd9c1635f4705
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-03-07 16:09:19 -0800

    add test Truck Tour

---
+++ src/test/java/com/github/quiram/hackerrank/tests/intermediate/trucktour/ResultTest.java
@@ -0,0 +1,25 @@
+package com.github.quiram.hackerrank.tests.intermediate.trucktour;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ResultTest {
+
+    @ParameterizedTest
+    @MethodSource("scenarios")
+    void truckTour(List<List<Integer>> petrolpumps, int result) {
+        assertEquals(result, Result.truckTour(petrolpumps));
+    }
+
+    public static Stream<Arguments> scenarios() {
+        return Stream.of(
+                Arguments.of(List.of(List.of(1, 5), List.of(10, 3), List.of(3, 4)), 1)
+        );
+    }
+}
