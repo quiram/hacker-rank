748787fcdd29275dc38e74cdbce2a2b80f95bb8f
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-03-11 16:30:23 -0700

    add test Lego Blocks, working for n=1

---
+++ src/main/java/com/github/quiram/hackerrank/intermediate/legoblocks/Result.java
@@ -0,0 +1,67 @@
+package com.github.quiram.hackerrank.intermediate.legoblocks;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Result {
+
+    /*
+     * Complete the 'legoBlocks' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+
+    private static Set<String> combinations;
+
+    public static int legoBlocks(int n, int m) {
+        combinations = new HashSet<>();
+
+        // Write your code here
+        //  n = 2;
+        generate(0, new StringBuilder(), m);
+
+        if (n == 1) {
+            return combinations.size();
+        }
+
+        int count = 0;
+
+        for (String c1 : combinations) {
+            for (String c2 : combinations) {
+                int p1 = 0;
+                int p2 = 0;
+                boolean compatible = true;
+                for (int i = 0; i < c1.length() && i < c2.length() && compatible; i++) {
+                    p1 += Integer.parseInt("" + c1.charAt(i));
+                    p2 += Integer.parseInt("" + c2.charAt(i));
+                    compatible = p1 != p2;
+                }
+                if (compatible) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static void generate(int partial, StringBuilder sb, int m) {
+        if (partial == m) {
+            combinations.add(sb.toString());
+            sb.delete(sb.length() - 1, sb.length());
+        }
+
+        if (partial > m) {
+            sb.delete(sb.length() - 1, sb.length());
+            return;
+        }
+
+        for (int i = 1; i <= 4; i++) {
+            generate(partial + i, sb.append(i), m);
+        }
+    }
+
+}
---
+++ src/test/java/com/github/quiram/hackerrank/intermediate/legoblocks/ResultTest.java
@@ -0,0 +1,24 @@
+package com.github.quiram.hackerrank.intermediate.legoblocks;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ResultTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,1",
+            "1,2,2",
+            "1,3,4",
+            "2,2,3",
+            "2,3,9",
+            "3,2,7",
+            "4,4,3375"
+    })
+    void legoBlocks(int n, int m, int result) {
+        final int actual = Result.legoBlocks(n, m);
+        assertEquals(result, actual);
+    }
+}
